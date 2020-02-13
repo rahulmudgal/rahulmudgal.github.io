248d46125513e2f4b9fe3985098a824b51740be5
Rahul <rahulmudgal1996@gmail.com>
2020-02-13 14:50:22 +0530

    service wroker file

--- assets/custom.js
+++ assets/custom.js
@@ -22,3 +22,12 @@ $(document).ready(function () {
         } // End if
     });
 });
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
+            .then((reg) => {
+                console.log('Service worker registered.', reg);
+            });
+    });
+}
