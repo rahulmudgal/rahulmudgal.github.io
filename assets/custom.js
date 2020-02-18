fff7870dfbf9557164c7c8712ea7869b9dec01bb
Rahul <rahulmudgal1996@gmail.com>
2020-02-18 14:59:58 +0530

    updated serviceWorker

--- assets/custom.js
+++ assets/custom.js
@@ -31,3 +31,15 @@ if ('serviceWorker' in navigator) {
             });
     });
 }
+
+/* Counter Code */
+$('span.counter').each(function () {
+    var $this = $(this);
+    jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+        duration: 3000,
+        easing: 'swing',
+        step: function () {
+            $this.text(Math.ceil(this.Counter));
+        }
+    });
+});
--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,15 @@
 const FILES_TO_CACHE = [
     '/index.html',
 ];
-
-evt.waitUntil(
-    caches.keys().then((keyList) => {
-        return Promise.all(keyList.map((key) => {
-            if (key !== CACHE_NAME) {
-                console.log('[ServiceWorker] Removing old cache', key);
-                return caches.delete(key);
-            }
-        }));
-    })
-);
+self.addEventListener('install', evt => {
+    evt.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+})
