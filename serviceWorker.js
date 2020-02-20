e61b54f07fa683ed532d32dd262af8df237f80c1
Rahul <rahulmudgal1996@gmail.com>
2020-02-20 12:19:50 +0530

    rename serviceWorker

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,12 +1,13 @@
 const FILES_TO_CACHE = [
     '/index.html',
 ];
+
 self.addEventListener('install', evt => {
     evt.waitUntil(
         caches.keys().then((keyList) => {
             return Promise.all(keyList.map((key) => {
                 if (key !== CACHE_NAME) {
-                    console.log('[ServiceWorker] Removing old cache', key);
+                    console.log('ServiceWorker: Removing old cache', key);
                     return caches.delete(key);
                 }
             }));
